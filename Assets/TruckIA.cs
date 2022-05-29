9e13caa926bf78f568cb21ed33e52930f0290b0e
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-05-29 17:35:02 -0300

    FirstExperiment

---
+++ Assets/TruckIA.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckIA : MonoBehaviour
+{
+    public float movementSpeed;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, movementSpeed);
+    }
+    
+    void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Prueba")
+        {
+            player.transform.Translate(0, 0, movementSpeed);
+       }
+    }
+}
