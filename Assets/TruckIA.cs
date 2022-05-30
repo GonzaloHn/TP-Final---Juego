1f2d44d81fc7e5d18cbb35baf369f6299bf4c6d1
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-05-30 14:29:26 -0300

    PracticeTime

--- Assets/TruckIA.cs
+++ Assets/TruckIA.cs
@@ -15,7 +15,7 @@ public class TruckIA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, movementSpeed);
+        transform.Translate(movementSpeed, 0, 0);
     }
     
     void OnCollisionStay(Collision collision)
@@ -26,4 +26,41 @@ public class TruckIA : MonoBehaviour
             player.transform.Translate(0, 0, movementSpeed);
        }
     }
+
+    /*public GameObject Bullet;
+     public Transform BulletSpawn;
+     public float reloadTime;
+     float currReloadTime;
+     bool canShoot = True;
+     void Start(){
+     currReloadTime = reloadTime
+     }
+
+     void update(){
+     if(currReloadTime > 0){
+     currReloadTime -= Time.deltaTime;
+     Debug.Log(reloadTime);
+     }
+
+     if(GetMouseButton(0) && reloadTime <= 0){
+
+     Instantiate(Bullet, BulletSpawn.position, BulletSpawn.rotation);
+     currReloadTime = reloadTime;
+     }
+     Debug.Log()
+
+     
+     Esto en el Bullet:
+     public float speed;
+     public float lifetime;
+
+     void Start(){
+     Destroy(gameObject, lifetime)
+    }
+     void Update(){
+     transfomr.position += transform.forward * speed;}
+     
+     void OnCollisionEnter(){
+     Destroy(gameObject);
+    }*/
 }
